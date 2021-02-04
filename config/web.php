d1f45b82fe53aa7dfdfe299e77dece9c0d7cd864
Rafael Nasibullin <rnasibullin@bigland.ru>
2021-02-04 19:07:44 +0600

    install yiisoft/yii2-sphinx

--- config/web.php
+++ config/web.php
@@ -51,6 +51,12 @@ $config = [
             'rules' => [
             ],
         ],
+        'sphinx' => [
+            'class' => 'yii\sphinx\Connection',
+            'dsn' => 'mysql:host=127.0.0.1;port=9306;dbname=local_internet',
+            'username' => '',
+            'password' => '',
+        ],
     ],
     'params' => $params,
 ];
